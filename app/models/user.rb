b2434c571bdfb0371464e0d79e9fcc05f24d262c
InSutu <dpauli@insutu.com>
2015-03-23 17:10:36 -0700

    Add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  # Added to support user roles
+   def admin?
+     role == 'admin'
+   end
+   
+   def standard?
+     role == 'standard'
+   end
+
+   def premium?
+     role == 'premium'
+   end
+
 end
 
 #This was created by calling 'rails g devise User' - we used that to create a USer calss that was devise aware and would tie into all of the devise create user authentication views
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323223212) do
+ActiveRecord::Schema.define(version: 20150324000004) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150323223212) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
